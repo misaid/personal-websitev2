b2298b9feef8fc1d4dbbb14bd5cc220131bec496
misaid <mo-45@hotmail.com>
2024-10-19 22:23:01 -0600

    feat: added a footer

--- app/page.tsx
+++ app/page.tsx
@@ -103,11 +103,43 @@ export default function Home(): JSX.Element {
             <div className="w-full p-4 h-64 rounded-lg border"></div>
           </div>
 
-          {/* Footer */}
+          {/* notice */}
           <div className="w-full max-w-[700px] mx-5 p-4 space-y-2.5 shadow-md rounded-lg border mt-5 flex justify-center">
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight ">
               🚧 SITE UNDER CONSTRUCTION 🚧
             </h3>
+          </div>
+
+          <div className="w-full max-w-[700px] mx-5 p-4 space-y-2.5 rounded-lg mt-5 flex items-center">
+            <div className="w-full">
+              <h4 className="text-sm text-muted-foreground">
+                © 2024 mohamedsaid.ca |{" "}
+                <span className="font-semibold">privacy</span>
+              </h4>
+            </div>
+
+            <div className="flex w-full justify-end items-center">
+              <div className="flex flex-row space-x-2">
+                <a
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Linkedin />
+                </a>
+
+                <a
+                  href="https://www.github.com/misaid"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github />
+                </a>
+                <a href="mailto:mohamediasaid@outlook.ca">
+                  <Mail />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
